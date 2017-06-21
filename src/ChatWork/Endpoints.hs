fbb4ade39c7505bcf76e7bbb871d847246c2559b
Nobutada Matsubara <nobutada.matsubara.bigmoon@gmail.com>
2017-06-21 17:43:12 +0900

    Fixed endpoint functions
    when my tasks is empty, the following error occurred.
    
    >> print =<< (responseBody <$> getMyTasks "xxxx")
    *** Exception: JsonHttpException "Error in $: not enough input"

--- src/ChatWork/Endpoints.hs
+++ src/ChatWork/Endpoints.hs
@@ -4,10 +4,24 @@
 module ChatWork.Endpoints
     ( baseUrl
     , mkTokenHeader
+    , getHttpResponse'
+    , fixEmptyStringManager
+    , fixEmptyString
     ) where
 
+import Data.Default.Class (def)
+import Data.List (lookup)
+import Data.Maybe (fromMaybe)
+import Data.Proxy (Proxy(..))
+import Control.Monad.IO.Class (MonadIO(..))
+import Network.Connection (initConnectionContext)
+import Network.HTTP.Client ( Request, Manager, Response(..), BodyReader
+                           , ManagerSettings(..), newManager)
+import Network.HTTP.Client.Internal (constBodyReader)
+import Network.HTTP.Client.TLS (mkManagerSettingsContext)
 import Network.HTTP.Req ( HttpResponse(..), Url, Scheme(Https), Option
-                        , https, (/:), header)
+                        , https, (/:), header, MonadHttp)
+import Network.HTTP.Types.Header (hContentLength)
 import ChatWork.Types
 
 baseUrl :: Url 'Https
@@ -15,3 +29,19 @@ baseUrl = https "api.chatwork.com" /: "v2"
 
 mkTokenHeader :: Token -> Option 'Https
 mkTokenHeader token = header "X-ChatWorkToken" token
+
+getHttpResponse' :: (HttpResponse a, MonadHttp m) => Proxy a -> Request -> Manager -> m a
+getHttpResponse' Proxy r m = liftIO $ getHttpResponse r =<< fixEmptyStringManager
+
+fixEmptyStringManager :: IO Manager
+fixEmptyStringManager = do
+  context <- initConnectionContext
+  let settings = mkManagerSettingsContext (Just context) def Nothing
+  newManager $ settings { managerModifyResponse = fixEmptyString }
+
+fixEmptyString :: Response BodyReader -> IO (Response BodyReader)
+fixEmptyString res = do
+  reader <- constBodyReader ["[]"]
+  let
+    contentLength = fromMaybe "0" $ lookup hContentLength (responseHeaders res)
+  return $ if contentLength /= "0" then res else res { responseBody = reader }
--- src/ChatWork/Endpoints/My.hs
+++ src/ChatWork/Endpoints/My.hs
@@ -10,7 +10,7 @@ import ChatWork.Endpoints
 import ChatWork.Types
 
 getMyStatus :: (MonadHttp m) => Token -> m (JsonResponse GetMyStatusResponse)
-getMyStatus = req GET (baseUrl /: "my" /: "status") NoReqBody jsonResponse . mkTokenHeader
+getMyStatus = flip (req' GET (baseUrl /: "my" /: "status") NoReqBody) (getHttpResponse' jsonResponse) . mkTokenHeader
 
 getMyTasks :: (MonadHttp m) => Token -> m (JsonResponse GetMyTasksResponse)
-getMyTasks = req GET (baseUrl /: "my" /: "tasks") NoReqBody jsonResponse . mkTokenHeader
+getMyTasks = flip (req' GET (baseUrl /: "my" /: "tasks") NoReqBody) (getHttpResponse' jsonResponse) . mkTokenHeader
